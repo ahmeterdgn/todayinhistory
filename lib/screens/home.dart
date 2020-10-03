e4e734260916335218638a7fd0c3c9391b9c3596
Ahmet Erdoğan <48730205+ahmeterdgn@users.noreply.github.com>
2020-10-03 10:34:02 +0200

    flutter_inner_drawer package added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,6 +5,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:todayinhistory/constants/global.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_inner_drawer/inner_drawer.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -44,54 +45,122 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  bool _swipe = true;
+  InnerDrawerAnimation _animationType = InnerDrawerAnimation.static;
+  bool _proportionalChildArea = true;
+  double _horizontalOffset = 0.8;
+  double _verticalOffset = 0.8;
+  double _scale = 0.9;
+  double _borderRadius = 50;
+  Color currentColor = Colors.red;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('TIH'),
+    return InnerDrawer(
+      onTapClose: true,
+      offset: IDOffset.only(
+        bottom: _verticalOffset,
+        right: _horizontalOffset,
+        left: _horizontalOffset,
       ),
-      bottomNavigationBar: ConvexAppBar(
-        backgroundColor: Colors.red[400],
-        elevation: 5,
-        // cornerRadius: 5,
-        items: [
-          TabItem(
-            icon: FontAwesomeIcons.bookDead,
-            title: 'Deaths',
-          ),
-          TabItem(
-            icon: Icons.event,
-            title: 'Events',
-          ),
-          TabItem(
-            icon: FontAwesomeIcons.birthdayCake,
-            title: 'Birthday',
-          ),
-        ],
-        initialActiveIndex: 1, //optional, default as 0
-        onTap: (int i) {
-          setState(() {
-            bolum = i == 1 ? 'Events' : i == 2 ? 'Births' : 'Deaths';
-            data.clear();
-            fetch();
-          });
-        },
-      ),
-      body: ListView.builder(
-          padding: EdgeInsets.fromLTRB(0, 0, 0, 25),
-          itemCount: data.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Year : ${data[index]['year']}'),
-              subtitle: Text(data[index]['text']),
-              trailing: IconButton(
-                icon: Icon(Icons.open_in_new),
+      scale: IDOffset.horizontal(_scale),
+      borderRadius: _borderRadius,
+      duration: Duration(milliseconds: 11200),
+      swipe: _swipe,
+      proportionalChildArea: _proportionalChildArea,
+      colorTransitionChild: currentColor,
+      leftAnimationType: _animationType,
+      rightAnimationType: _animationType,
+      leftChild: Material(
+          color: Colors.red[400],
+          child: Center(
+            child: Container(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Image",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "Image",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "Image",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "Image",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "Image",
+                  style: TextStyle(fontSize: 18),
+                ),
+                Text(
+                  "Image",
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            )),
+          )),
+      scaffold: Scaffold(
+        appBar: AppBar(
+          title: Text('TIH'),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.menu),
                 onPressed: () {
-                  _launchURL(url: data[index]['link'][0]['link']);
-                },
-              ),
-            );
-          }),
+                  setState(() {
+                    _animationType = InnerDrawerAnimation.static;
+                  });
+                })
+          ],
+        ),
+        bottomNavigationBar: ConvexAppBar(
+          backgroundColor: Colors.red[400],
+          elevation: 5,
+          // cornerRadius: 5,
+          items: [
+            TabItem(
+              icon: FontAwesomeIcons.bookDead,
+              title: 'Deaths',
+            ),
+            TabItem(
+              icon: Icons.event,
+              title: 'Events',
+            ),
+            TabItem(
+              icon: FontAwesomeIcons.birthdayCake,
+              title: 'Birthday',
+            ),
+          ],
+          initialActiveIndex: 1, //optional, default as 0
+          onTap: (int i) {
+            setState(() {
+              bolum = i == 1 ? 'Events' : i == 2 ? 'Births' : 'Deaths';
+              data.clear();
+              fetch();
+            });
+          },
+        ),
+        body: ListView.builder(
+            padding: EdgeInsets.fromLTRB(0, 0, 0, 25),
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text('Year : ${data[index]['year']}'),
+                subtitle: Text(data[index]['text']),
+                trailing: IconButton(
+                  icon: Icon(Icons.open_in_new),
+                  onPressed: () {
+                    _launchURL(url: data[index]['link'][0]['link']);
+                  },
+                ),
+              );
+            }),
+      ),
     );
   }
 
